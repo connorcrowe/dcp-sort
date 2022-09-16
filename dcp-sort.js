a2db989356a835bd098e90a6c9346a7498d97e76
Connor Crowe <connor.haz.crowe@gmail.com>
2022-09-16 15:26:08 -0400

    Add function to merge together sorted chunks. Add main and requirements

--- dcp-sort.js
+++ dcp-sort.js
@@ -2,7 +2,8 @@
  * DCP-SORT
  * @file    dcp-sort.js
  * 
- *          A distributed sorting algorithm, designed to sort large sets of numbers leveraging a massively parallel system, the Distributed Compute Protocol (DCP).
+ *          Originally written as a tutorial to demonstrate the Distributed Compute Protocol (DCP) in 2021 on docs.dcp.dev
+ *          A distributed sorting algorithm, designed to sort large sets of numbers leveraging a massively parallel system, DCP.
  *          It works by splitting the dataset into chunks of roughly equal sizes, and performing the default sort (usually TimSort) on each chunk in parallel on
  *          other machines (using DCP). The returning sorted chunks can then be merged greedily to create a large sorted list. Time and space complexity analysis will
  *          be included in the README
@@ -42,3 +43,64 @@ async function distributedWorkFunction(arr)
         return a - b;
     });
 }
+
+/* MERGE CHUNKS
+ * Takes sorted chunks and greedily combines them to return one sorted array */
+function mergeChunks(arr)
+{
+    const sorted = []
+    while(arr.length) 
+    {
+        let currentChunk = arr[0];
+        let bestChunkIndex = 0;
+        let bestValue = currentChunk[0];
+
+        for (let i = 0; i < arr.length; i += 1)
+        {
+            currentChunk = arr[i];
+            if (currentChunk[0] < bestValue)
+            {
+                [ bestValue ] = currentChunk;
+                bestChunkIndex = i;
+            }
+        }
+
+        sorted.push(arr[bestChunkIndex].shift());
+        if (arr[bestChunkIndex].length === 0) arr.splice(bestChunkIndex, 1);
+    }
+    return sorted;
+}
+
+/* MAIN
+ * Generate input (or provide one), split it, then use dcp-client to run the work function on the chunks. Finally, merge the chunks and test the output. */
+async function main() 
+{
+    const compute = require('dcp/compute');
+    const CHUNKS = 10
+    const TOTAL = CHUNKS * 2500
+
+    // Create input
+    let inputSet = splitInput(generateInput(TOTAL), CHUNKS);
+
+    const job = compute.for(inputSet, distributedWorkFunction);
+    const startTime = Date.now()
+    job.public.name = 'DCP-SORT'
+    //job.computeGroups = [{}]; comment out for security
+
+    job.on('accepted', () => {
+        console.log(`Job accepted at ${Math.round((Date.now() - startTime) / 100) /10} with ${job.id}`)
+    })
+
+    let resultSet = await job.exec();
+    resultSet = Array.from(resultSet);
+    console.log(`Time: ${Math.round((Date.now() - startTime) / 100) /10}. Parallel sorting of chunks complete`);
+
+    resultSet = mergeChunks(resultSet);
+    console.log(`Time: ${Math.round((Date.now() - startTime) / 100) /10}. Merging chunks complete`);
+}
+
+require('dcp-client')
+  .init('https://scheduler.distributed.computer')
+  .then(main)
+  .catch(console.error)
+  .finally(process.exit);
